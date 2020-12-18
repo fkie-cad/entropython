f25f2da476cf79c674e17dffc663973cdf0d3114
Enkelmann <nils-edvin.enkelmann@fkie.fraunhofer.de>
2020-12-18 14:24:25 +0100

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # entropython
-Efficient calculation of shannon byte entropy based on rust library.
+
+Efficient calculation of Shannon byte entropy based on Rust.
+
+## Installation
+
+For building the binary, [Rust](https://www.rust-lang.org/) needs to be installed.
+
+Run
+```shell
+cargo build --release
+mv target/release/libentropython.so entropython.so
+```
+
+The built `entropython.so` itself has no dependencies.
+
+## Usage
+
+```python
+from entropython import *
+
+bytes = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'.encode('utf-8')
+print(shannon_entropy(bytes))
+print(metric_entropy(bytes))
+```
---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+/// Compute the Shannon byte entropy of a byte array.
+#[pyfunction]
+fn shannon_entropy(bytes: &[u8]) -> f64 {
+    let mut byte_count = [0u64; 256];
+    for byte in bytes {
+        byte_count[*byte as usize] += 1;
+    }
+    let mut entropy = 0f64;
+    for counted_num in byte_count.iter().filter(|num| **num > 0u64) {
+        let byte_probability = *counted_num as f64 / (bytes.len() as f64);
+        entropy -= byte_probability * byte_probability.log2();
+    }
+
+    entropy
+}
+
+/// Compute the metric entropy of a byte array.
+///
+/// Metric entropy = Shannon entropy / 8
+#[pyfunction]
+fn metric_entropy(bytes: &[u8]) -> f64 {
+    shannon_entropy(bytes) / 8f64
+}
+
+#[pymodule]
+fn entropython(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(shannon_entropy, m)?)?;
+    m.add_function(wrap_pyfunction!(metric_entropy, m)?)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn shannon() {
+        let mut bytes = [0u8; 1000];
+        println!("shannon_entropy: {}", shannon_entropy(&bytes));
+        assert_eq!(shannon_entropy(&bytes), 0f64);
+
+        for i in 0..bytes.len() {
+            bytes[i] = i as u8;
+        }
+        println!("shannon_entropy: {}", shannon_entropy(&bytes));
+        assert!(shannon_entropy(&bytes) > 7.99f64);
+        assert!(shannon_entropy(&bytes) <= 8f64);
+    }
+}
