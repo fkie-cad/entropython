90d3a169808837919adc93250e64082289d946ba
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2026-01-20 10:57:16 +0100

    chore: updated python versions and pyo3
    * added wheels for new stable Python versions 3.13 and 3.14
    * removed wheels for EoL versions 3.8 and 3.9
    * switched to new pyo3 version 0.27.2

--- _build-wheels.sh
+++ _build-wheels.sh
@@ -6,16 +6,16 @@ ARCH=$(uname -m)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
-for PYBIN in /opt/python/cp{38,39,310,311,312}*/bin; do
+for PYBIN in /opt/python/cp{310,311,312,313,314}*/bin; do
     rm -rf /io/build/
     "${PYBIN}/pip" install -U setuptools setuptools-rust wheel
     "${PYBIN}/pip" wheel /io/ -w /io/dist/ --no-deps
 done
 
-for whl in /io/dist/*{cp38,cp39,cp310,cp311,cp312}*_"${ARCH}".whl; do
+for whl in /io/dist/*{cp310,cp311,cp312,cp313,cp314}*_"${ARCH}".whl; do
     auditwheel repair "$whl" -w /io/dist/
 done
 
-for PYBIN in /opt/python/cp{38,39,310,311,312}*/bin; do
+for PYBIN in /opt/python/cp{310,311,312,313,314}*/bin; do
     "${PYBIN}/pip" install entropython -f /io/dist/
 done
