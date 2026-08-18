16617c0a909a173e91755b61122acf88ad7650f2
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2026-08-18 17:45:29 +0200

    fix: fix for broken wheels missing files
    * fixes the problem, that __init__.py is missing from some of the built wheels
    * updated build scripts and build containers for better separation of individual builds

--- build-release.sh
+++ build-release.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -eux
 
+rm -rf dist/ build/ target/ *.egg-info
+
 # create release for multiple python versions with manylinux
-docker run --rm -v "$(pwd)":/io quay.io/pypa/manylinux2014_x86_64 bash /io/_build-wheels.sh
+docker run --rm -u "$(id -u):$(id -g)" -v "$(pwd)":/io \
+    quay.io/pypa/manylinux_2_28_x86_64 bash /io/_build-wheels.sh
 
 # also create a source release with build:
-python3 -m pip install -U build
-python3 -m build --sdist
+uv build --sdist --out-dir dist/
 
 # finally do a cross build for arm64:
 # For this to work, qemu must be registered with docker. You can do so by running
 # docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 # (see also: https://github.com/multiarch/qemu-user-static)
-docker run --rm -v "$(pwd)":/io quay.io/pypa/manylinux2014_aarch64 bash /io/_build-wheels.sh
+docker run --rm -u "$(id -u):$(id -g)" -v "$(pwd)":/io \
+    quay.io/pypa/manylinux_2_28_aarch64 bash /io/_build-wheels.sh
